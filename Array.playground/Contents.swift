8758972526c0e75b0eb3f51ceb3bbf66b38182b1
doulos76 <inojyes@gmail.com>
2019-05-03 23:52:07 +0900

    Add array and dictionary swift standard data type

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//: Array
+
+var people = ["Brian", "Stanley", "Ringo"]
+
+//: Order
+people[0]
+people[1]
+people[2]
+
+//: Insertion Location
+
+people.append("Charles")
+print(people)
+
+people.insert("Andy", at: 0)
---
+++ Dictionary.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+//: # Dictionary
+
+var scores: [String: Int] = ["Eric": 9, "Mark": 12, "Wayne": 1]
+scores["Andrew"] = 0
+
+scores
